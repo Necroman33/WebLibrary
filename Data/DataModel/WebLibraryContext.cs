ff9f1857fbd951ffa8f5cbbee1579f6506012012
Necroman33 <57381978+Necroman33@users.noreply.github.com>
2021-01-29 05:44:42 +0300

    application frame

---
+++ Data/DataModel/WebLibraryContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.DataModel
+{
+    public class WebLibraryContext: DbContext
+    {
+        public WebLibraryContext(DbContextOptions<WebLibraryContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
